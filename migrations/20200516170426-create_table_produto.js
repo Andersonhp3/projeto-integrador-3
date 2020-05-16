55f56cfb78c10eb11a1d763b3046c7fda5b594c7
Alex Silva <60552754+alexsilva11@users.noreply.github.com>
2020-05-16 14:09:11 -0300

    Criada a migration da tabela produto

---
+++ migrations/20200516170426-create_table_produto.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('produto', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      nome: {
+        type: Sequelize.STRING(100),
+        allowNull: false
+      },
+      preco: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+      },
+      descricao: {
+        type: Sequelize.STRING(200),
+        allowNull: false
+      },
+      estoque: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      categoria_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'categoria_pet',
+          key: 'id'
+        },
+        allowNull: false,
+      },
+      usuario_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'usuario_id',
+          key: 'id'
+        },
+        allowNull: false,
+      }
+    })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('produto');
+  }
+};
