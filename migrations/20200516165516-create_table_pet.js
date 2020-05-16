ca0f0e3671510775a03f6837953b4183dec1a698
Alex Silva <60552754+alexsilva11@users.noreply.github.com>
2020-05-16 14:02:48 -0300

    Criada a migration da tabela pet

---
+++ migrations/20200516165516-create_table_pet.js
@@ -0,0 +1,55 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('pet', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      nome: {
+        type: Sequelize.STRING(100),
+        allowNull: false
+      },
+      idade: {
+        type: Sequelize.INTEGER(2),
+        allowNull: false
+      },
+      descricao: {
+        type: Sequelize.STRING(200),
+        allowNull: false
+      },
+      contato: {
+        type: Sequelize.STRING(45),
+        allowNull: false
+      },
+      raca: {
+        type: Sequelize.STRING(45),
+      },
+      categoria_pet_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'categoria_pet',
+          key: 'id'
+        },
+        allowNull: false,
+      },
+      usuario_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'usuario_id',
+          key: 'id'
+        },
+        allowNull: false,
+      }
+    })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    
+    return queryInterface.dropTable('pet');
+
+  }
+};
