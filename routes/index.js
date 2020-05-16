b21eade96e030d6ccf8ceb1a4bb4c080f4f4ef36
Anderson Nicacio <43139753+anicacio@users.noreply.github.com>
2020-05-16 15:50:25 -0300

    Criado cadastroController

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,19 @@ const router = express.Router();
 /* Controllers */
 const homeController = require('../controllers/homeController');
 const loginController = require('../controllers/loginController');
+const cadastroController = require('../controllers/cadastroController');
 
 
-/* GET home page. */
+
+/* GET Home */
 router.get('/home', homeController.home);
 
 /* GET Login */
 router.get('/login', loginController.view);
+
+/* GET Cadastro */
+router.get('/cadastro', cadastroController.view);
+
 
 
 module.exports = router;
