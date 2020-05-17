83ef603eb0af43cfbdd3a16c964b523a8757d5d0
Anderson Nicacio <43139753+anicacio@users.noreply.github.com>
2020-05-17 12:22:25 -0300

    Revert "Criado entrade de cadastro"
    This reverts commit 7be3b46ba4de98dd4ad6000c0808c75453185e0b.

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,29 +2,23 @@ const path = require('path');
 const fs = require('fs');
 const bcrypt = require('bcrypt');
 
-
 const authController = {
     login: (req, res) => {
 
     },
 
-    cadastro: async (req, res, next) => {
-        console.log(req.body);
-        console.log(req.file);
-        if(req.file){
-            return res.send(req.file);
-        }
-        return res.send('Houve erro no upload!');
+    cadastro: (req, res) => {
+
     },
 
-    showLogin: (req, res) => {
+    viewLogin: (req, res) => {
         res.render('login', {
             title: 'Tela de Login',
             error: false
         });
     },
 
-    showCadastro: (req, res) => {
+    viewCadastro: (req, res) => {
         res.render('cadastro', {
             title: 'Tela de Cadastro',
             error: false
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const multer = require('../middlewares/multerProfile');
 
 /* Controllers */
 const homeController = require('../controllers/homeController');
 const authController = require('../controllers/authController');
 
+
+
 /* homeController */
 router.get('/home', homeController.home);
 
 /* authController */
-router.get('/login', authController.showLogin);
-router.get('/cadastro', authController.showCadastro);
-router.post('/cadastro', multer.single('imagem'), authController.cadastro);
+router.get('/login', authController.viewLogin);
+router.get('/cadastro', authController.viewCadastro);
+
+
 
 module.exports = router;
