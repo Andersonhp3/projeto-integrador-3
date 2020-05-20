b0037ac5e7d4ca0b33a2708729abc04d342d0de4
Andersonhp3 <arhp1591@gmail.com>
2020-05-20 20:31:59 -0300

    Adicionando model Estado

---
+++ models/Estado.js
@@ -0,0 +1,29 @@
+let Estado = (sequelize, DataTypes) => {
+    let estado = sequelize.define(
+        "Estado", {
+            id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true
+            },
+            estado: {
+                type: DataTypes.STRING(2),
+                allowNull: false
+            },
+        }, {
+            tableName: "estado",
+            timestamps: false
+        }
+    );
+
+    estado.associate = (models) => {
+        estado.hasMany(models.Cidade, {
+            as: "cidade"
+        })
+    }
+
+    return estado;
+}
+
+module.exports = Estado;
