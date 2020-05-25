8be9918603e180f84a37270bc18b8004161237e7
Andersonhp3 <arhp1591@gmail.com>
2020-05-25 14:36:09 -0300

    Adicionando mais categorias

--- seeders/20200525165200-seeder_categoria.js
+++ seeders/20200525165200-seeder_categoria.js
@@ -5,19 +5,79 @@ module.exports = {
 
     return queryInterface.bulkInsert('categoria', [{
         id: 1,
-        categoria: 'Cachorro'
+        categoria: 'Alimentação',
       },
       {
         id: 2,
-        categoria: 'Gatos'
+        categoria: 'Farmácia'
       },
       {
         id: 3,
-        categoria: 'Pássaros'
+        categoria: 'Higiene'
       },
       {
         id: 4,
-        categoria: 'Hamster'
+        categoria: 'Camas'
+      },
+      {
+        id: 5,
+        categoria: 'Brinquedos'
+      },
+      {
+        id: 6,
+        categoria: 'Casinhas e Acessórios'
+      },
+      {
+        id: 7,
+        categoria: 'Passeio'
+      },
+      {
+        id: 8,
+        categoria: 'Adestramento'
+      },
+      {
+        id: 9,
+        categoria: 'Roupas e Acessórios'
+      },
+      {
+        id: 10,
+        categoria: 'Arranhadores'
+      },
+      {
+        id: 11,
+        categoria: 'Transporte'
+      },
+      {
+        id: 12,
+        categoria: 'Coleiras'
+      },
+      {
+        id: 13,
+        categoria: 'Comedouros e Bebedouros'
+      },
+      {
+        id: 14,
+        categoria: 'Fonte para gatos'
+      },
+      {
+        id: 15,
+        categoria: 'Aquário'
+      },
+      {
+        id: 16,
+        categoria: 'Bombas e Compressores'
+      },
+      {
+        id: 17,
+        categoria: 'Filtros'
+      },
+      {
+        id: 18,
+        categoria: 'Gaiolas Hamster'
+      },
+      {
+        id: 19,
+        categoria: 'Bombas e Compressores'
       },
     ], {});
 
