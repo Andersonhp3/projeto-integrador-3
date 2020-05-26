8016cb75ae63e5af494fc232b8f79f5497145697
Andersonhp3 <arhp1591@gmail.com>
2020-05-26 10:35:01 -0300

    Correções

--- seeders/20200525181823-seeder_produto.js
+++ seeders/20200525181823-seeder_produto.js
@@ -7,7 +7,7 @@ module.exports = {
       id: 1,
       nome: 'Cama para cão',
       preco: 49.99,
-      descricao: 'Cama para cães e gatos nº1 com almofada e cesto ótima opção para o descanso do seu melhor amigo. Essa cama é confeccionada em plástico e traz ótimo custo benefício para você.',
+      descricao: 'Cama para cães e gatos nº1 com almofada e cesto',
       estoque: 5,
       categoria_id: 4,
       usuario_id: 1
@@ -34,7 +34,7 @@ module.exports = {
       id: 4,
       nome: 'Comedouro Pet em Aço Inox 2 Peças Tamanho M 250ml - Basic+',
       preco: 9.99,
-      descricao: 'Comedouro Pet em Aço Inox Tamanho M 250ml - Basic+.Imagens meramente ilustrativas.Todas as informações divulgadas são de responsabilidade do Fabricante/Fornecedor',
+      descricao: 'Comedouro Pet em Aço Inox Tamanho M 250ml - Basic+.Imagens meramente ',
       estoque: 40,
       categoria_id: 13,
       usuario_id: 2
@@ -43,7 +43,7 @@ module.exports = {
       id: 5,
       nome: 'Casinha para Cachorro Decorativa Pet Pequeno - Meemo',
       preco: 89.99,
-      descricao: 'A Casinha decorativa serve como abrigo para seu animalzinho e também como mesa decorativa. Com Meemo seu melhor amigo terá uma vida saudável e com muito estilo. É impermeável, inodoro e isola o calor. Todas as informações divulgadas...',
+      descricao: 'A Casinha decorativa serve como abrigo para seu animalzinho',
       estoque: 30,
       categoria_id: 6,
       usuario_id: 7
@@ -52,7 +52,7 @@ module.exports = {
       id: 6,
       nome: 'Casinha para Cachorro Decorativa Pet Pequeno - Meemo',
       preco: 69.99,
-      descricao: 'A Casinha decorativa serve como abrigo para seu animalzinho e também como mesa decorativa. Com Meemo seu melhor amigo terá uma vida saudável e com muito estilo. É impermeável, inodoro e isola o calor. Todas as informações divulgadas...',
+      descricao: 'A Casinha decorativa serve como abrigo para seu animalzinho',
       estoque: 30,
       categoria_id: 6,
       usuario_id: 8
@@ -61,7 +61,7 @@ module.exports = {
       id: 7,
       nome: 'Roupa Casacos Poliéster Cão Gato Pet Elegante',
       preco: 29.99,
-      descricao: 'Roupa Casacos Poliéster Cão Gato Pet EleganteCasacos Roupa para Cão, Gato, Pet.Confeccionado em Poliéster, soft e microfibra para seu bebe com muito amor e carinho para o friozinho....',
+      descricao: 'Roupa Casacos Poliéster Cão Gato Pet EleganteCasacos Roupa para Cão, Gato',
       estoque: 20,
       categoria_id: 9,
       usuario_id: 10
@@ -70,7 +70,7 @@ module.exports = {
       id: 8,
       nome: 'Roupa Casacos Poliéster Cão Gato Pet Elegante 2',
       preco: 69.99,
-      descricao: 'Roupa Casacos Poliéster Cão Gato Pet EleganteCasacos Roupa para Cão, Gato, Pet.Confeccionado em Poliéster, soft e microfibra para seu bebe com muito amor e carinho para o friozinho....',
+      descricao: 'Roupa Casacos Poliéster Cão Gato Pet EleganteCasacos Roupa para Cão, Gato',
       estoque: 20,
       categoria_id: 9,
       usuario_id: 10
@@ -79,16 +79,16 @@ module.exports = {
       id: 9,
       nome: 'Roupa Casacos Poliéster Cão Gato Pet Elegante 4',
       preco: 109.99,
-      descricao: 'Roupa Casacos Poliéster Cão Gato Pet EleganteCasacos Roupa para Cão, Gato, Pet.Confeccionado em Poliéster, soft e microfibra para seu bebe com muito amor e carinho para o friozinho....',
+      descricao: 'Roupa Casacos Poliéster Cão Gato Pet EleganteCasacos Roupa para Cão, Gato',
       estoque: 20,
       categoria_id: 9,
       usuario_id: 10
     },
     {
       id: 10,
-      nome: 'Tipo de extensão do assento Pet Car Belt Dog ajustável Leash Cat Reflective Segurança Traction Belt',
+      nome: 'Tipo de extensão do assento Pet Car Belt Dog ajustável Leash',
       preco: 19.99,
-      descricao: 'Descrição: Tipo de Item: Pet Car Cinto de Segurança Aplicável: Pet Cor: preto, verde, cinza Produtos peso líquido: 0,093 kg Tamanho do produto: 2.5 * 53-74cm, comprimento máximo elástica: 78 centímetros Características - Material de..',
+      descricao: 'Descrição: Tipo de Item: Pet Car Cinto de Segurança Aplicável',
       estoque: 10,
       categoria_id: 11,
       usuario_id: 10
@@ -97,16 +97,16 @@ module.exports = {
       id: 11,
       nome: 'Madeira de Bell brinquedo bola de Pequenos Animais Coelho Guin¨¦ Hamster Pet Rat Hot Dog',
       preco: 109.99,
-      descricao: 'Madeira de Bell brinquedo bola de Pequenos Animais Coelho Guin¨¦ Hamster Pet Rat Hot DogDescri??o:100%novo e de alta qualidadeN?o t¨®xicomaterial, seguro para mastigar e jogar;Um bemabastecimento dos brinquedos para se divertir.',
+      descricao: 'Madeira de Bell brinquedo bola de Pequenos Animais Coelho Guin¨¦',
       estoque: 110,
       categoria_id: 5,
       usuario_id: 10
     },
     {
       id: 12,
-      nome: 'Ração Royal Canin Urinary S/O Veterinary Diet Canine cachorro raça média/grande mix 10.1kg',
+      nome: 'Ração Royal Canin Urinary S/O Veterinary Diet Canine cachorro 1',
       preco: 209.99,
-      descricao: 'Eles fazem parte da família e nos dão sua companhia e seu amor. Para que seu fiel amigo permaneça saudável e forte, uma dieta equilibrada é essencial. Dê ao seu cão todos os nutrientes que ele precisa para que ele possa correr, pular e brincar o dia todo.',
+      descricao: 'Eles fazem parte da família e nos dão sua companhia e seu amor. ',
       estoque: 210,
       categoria_id: 1,
       usuario_id: 6
@@ -115,7 +115,7 @@ module.exports = {
       id: 13,
       nome: 'Ração Royal Canin Hepatic Veterinary Diet Canine cachorro adulto 10kg',
       preco: 109.99,
-      descricao: 'Eles fazem parte da família e nos dão sua companhia e seu amor. Para que seu fiel amigo permaneça saudável e forte, uma dieta equilibrada é essencial. Dê ao seu cão todos os nutrientes que ele precisa para que ele possa correr, pular e brincar o dia todo.',
+      descricao: 'Eles fazem parte da família e nos dão sua companhia e seu amor. ',
       estoque: 110,
       categoria_id: 1,
       usuario_id: 6
@@ -133,7 +133,7 @@ module.exports = {
       id: 15,
       nome: 'Filtro Uv Esterilizador 36w Ocean Tech Aquários Fontes Lagos',
       preco: 299.99,
-      descricao: 'Especificações Do Filtro UV Ocean Tech 36W | Água verde nunca mais! Sistema de esterilização UV com design altamente eficiente e compacto para eliminação de algas e micro-organismos infecciosos prejudiciais aos habitantes do aquário.',
+      descricao: 'Especificações Do Filtro UV Ocean Tech 36W | Água verde nunca mais!',
       estoque: 40,
       categoria_id: 15,
       usuario_id: 6
@@ -142,7 +142,7 @@ module.exports = {
       id: 16,
       nome: 'Arranhador para Gatos com Três Bases Bege/Marrom Cat House - Basic+',
       preco: 399.99,
-      descricao: 'Permita que seu bichano libere seus instintos com o Arranhador para Gatos com Três Bases Bege/Marrom Cat House. Desenvolvido visando o bem estar e o desenvolvimento do seu gatinho, esse arranhador vai permitir que seu pet libere seu..',
+      descricao: 'Permita que seu bichano libere seus instintos com o Arranhador para Gatos com Três Bases Bege/Marrom Cat House.',
       estoque: 20,
       categoria_id: 5,
       usuario_id: 2
@@ -178,7 +178,7 @@ module.exports = {
       id: 20,
       nome: 'Glicopan Pet 30 mL',
       preco: 99.99,
-      descricao: 'SAÚDE E BEM-ESTAR - Estimula o apetite;- Excelente Energético;- Auxilia na melhora do estado geral de animais com estado nutricional inadequado;- Indicado no preparo de animais para exposição e em treinamento;- Complexo B;- Suplemen',
+      descricao: 'SAÚDE E BEM-ESTAR - Estimula o apetite;- Excelente Energético;- ',
       estoque: 100,
       categoria_id: 2,
       usuario_id: 3
@@ -187,7 +187,7 @@ module.exports = {
       id: 21,
       nome: 'Ciprovet Colírio Frasco - 5ml',
       preco: 99.99,
-      descricao: '- Indicado No Tratamento Das Seguintes Patologias Em Cães, Gatos E Equinos: Conjuntivite Infecciosa Bacteriana, Insuficiência Lacrimal, Ceratite Seca Contaminada E Úlcera De Córnea Provocada Por Micro-Organismos Gram Posi',
+      descricao: '- Indicado No Tratamento Das Seguintes Patologias Em Cães, Gatos E Equinos',
       estoque: 100,
       categoria_id: 2,
       usuario_id: 3
