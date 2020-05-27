21a6f2cda67cc1d6eb88140de739c254d2b0476b
Alex <alex.silva.11@hotmail.com>
2020-05-27 18:14:53 -0300

    Enviando informaçao do usuario logado para a pagina home

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -4,10 +4,13 @@ const fs = require('fs')
 
 const homeController = {
     home: (req, res) => {
+
+        let usuario = req.session.usuario
         res.render(
             'home', {
                 title: 'Home',
-                css: 'index'
+                css: 'index',
+                usuario
             }
         );
     }
