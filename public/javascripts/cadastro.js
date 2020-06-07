b8a8deeb5e2d188b55a452fad505397eb9f61b84
Anderson Nicacio <43139753+anicacio@users.noreply.github.com>
2020-06-07 01:05:25 -0300

    Adicionado Cropperjs para tratar imagem no Cadastr

---
+++ public/javascripts/cadastro.js
@@ -0,0 +1,43 @@
+// vars
+let upload = document.querySelector('#image-selector'),
+    result = document.querySelector("#canvas"),
+    save = document.querySelector("#pronto"),
+    preview = document.querySelector("#image-preview"),
+    cropper = "";
+
+// on change show image with crop options
+upload.addEventListener("change", (e) => {
+    if (e.target.files.length) {
+        // start file reader
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            if (e.target.result) {
+                // create new image
+                let img = document.createElement("img");
+                img.id = "image";
+                img.src = e.target.result;
+                // clean result before
+                result.innerHTML = "";
+                // append new image
+                result.appendChild(img);
+                // init cropper
+                cropper = new Cropper(img, {
+                    aspectRatio: 1,
+                    dragMode: 'move',
+                    minContainerWidth: 400,
+                    minContainerHeight: 400,
+                });
+            }
+        };
+        reader.readAsDataURL(e.target.files[0]);
+    }
+    $('#image-cropper').modal('show');
+});
+
+save.addEventListener('click', (e) => {
+    e.preventDefault();
+    let imgSrc = cropper
+    .getCroppedCanvas()
+    .toDataURL();
+    preview.src = imgSrc;
+});
