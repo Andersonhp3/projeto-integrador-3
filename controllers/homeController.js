3c1fed66e5ee007dd51d60314e4f0793fd7ae303
Andersonhp3 <arhp1591@gmail.com>
2020-06-13 19:26:19 -0300

    Mais vendidos pronto

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,19 +1,51 @@
 const path = require('path')
 const fs = require('fs')
 
+const {
+    Usuario,
+    Endereco,
+    Estado,
+    Cidade,
+    Pedido,
+    Produto,
+    Categoria,
+    ImagemProduto
+} = require("../models");
 
-const homeController = {
-    home: (req, res) => {
+
+
+const homeController =  {
+    home: async (req, res) =>  {
+
         let usuario = req.session.usuario
+
+
+        //Buscando todos itens
+        let itens = await Produto.findAll({
+            include: [{
+                model: ImagemProduto,
+                as: "imagem",
+                atributes: ["imagem"]
+            }]
+        });
+
+        let maisVendidos = itens.sort((a,b) => {
+            if(a.numero_vendas > b.numero_vendas) return -1;
+            if(a.numero_vendas < b.numero_vendas) return 1;
+            return 0;
+        })
+
+
         res.render(
             'home', {
                 title: 'Home',
                 css: 'index',
-                usuario
+                usuario,
+                maisVendidos
             }
         );
     }
-    
+
 }
 
 module.exports = homeController
