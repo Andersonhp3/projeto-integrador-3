c41e824e9547cb9de82b4d80b582707e66b54211
Anderson Nicacio <43139753+anicacio@users.noreply.github.com>
2020-06-14 20:39:11 -0300

    Add rotas /pet?id=XX e /pet/atualizar

--- routes/pet.js
+++ routes/pet.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const AdocaoController = require('../controllers/adocaoController');
 
+router.get('/', AdocaoController.detalhePet);
 router.get('/detalheAdocao', AdocaoController.detalheAdocao);
 router.get('/pesquisa', AdocaoController.pesquisa);
 router.post('/novo', AdocaoController.novaAdocao);
+router.post('/atualizar', AdocaoController.atualizar);
 
 module.exports = router;
