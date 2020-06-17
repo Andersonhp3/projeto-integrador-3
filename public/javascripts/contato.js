0ae67025e3c4ff9f24741310dae96b3bb4f40bb1
Andersonhp3 <arhp1591@gmail.com>
2020-06-17 00:53:00 -0300

    Adicionando mensagem de sucesso

--- public/javascripts/contato.js
+++ public/javascripts/contato.js
@@ -5,15 +5,20 @@ let email = document.getElementById("email");
 let mensagem = document.getElementById("mensagem");
 
 let alert = document.querySelector('.alert');
+let enviadoSucesso = document.querySelector('#enviadoSucesso');
 
 let btnEnviar = document.querySelector("#btnEnviar");
 
 let urlParams = new URLSearchParams(window.location.search);
 
+if(urlParams.has('sucesso')){
+    enviadoSucesso.style.display = 'block';
+}
+
 btnEnviar.addEventListener("click" , function(evento) {
 
     if(nome.value == '' || email.value == '' || mensagem == ''){
-        alert.style.display = 'block'
+        alert.style.display = 'block';
         evento.preventDefault();
     }
 
